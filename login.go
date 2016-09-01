5b415e725e62544198c3c54e2879c615aedeab94
thelilyliu <lilyxcliu@gmail.com>
2016-09-01 16:11:45 -0400

    Create structure to handle login

---
+++ login.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "gopkg.in/mgo.v2/bson"
+)
+
+/*
+  ========================================
+  Check
+  ========================================
+*/
+
+func checkPasswordDB(inputEmail, inputPassword string) (string, error) {
+    user := new(User)
+    
+    // create new MongoDB session
+    collection, session := mongoDBInitialization("user")
+    defer session.Close()
+    
+    // retrieve password field with email as selector
+    selector := bson.M{"email": inputEmail}
+    err := collection.Find(selector).Select(bson.M{"password": 1, "userid": 1}).One(user)
+    logErrorMessage(err)
+    
+    if inputPassword != user.Password { // incorrect password
+        user.UserID = "-1"
+    }
+    
+    return user.UserID, err
+}
