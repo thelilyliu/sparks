65a6fc2358e7643bfac3905f0a807332ca4776cc
thelilyliu <lilyxcliu@gmail.com>
2016-12-19 11:37:06 -0500

    Use CSS instead of JS to fade out pre-loader

--- assets/homepage/js/main.js
+++ assets/homepage/js/main.js
@@ -21,13 +21,13 @@
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+			/*	body.addClass('is-loading');
 
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
 				}, 250);
-			});
+			}); */
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
